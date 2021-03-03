fc21447c2a0f95326d328d1f1a6c622c6e311434
sanjanathota <sanjanat@vmware.com>
2021-03-03 14:45:13 +0530

    format

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,17 @@
 import react from 'react';
 import { connect } from 'react-redux';
 import {Link} from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem=({dispatch,id,description,amount,createdAt})=>{
     console.log("hmmm");
    return(<div>
         <p>{id}</p>
         <Link to={`/edit/${id}`}> <b>{description}</b></Link>      
-        <p>{amount}-{createdAt}</p>
+        <p>{numeral(amount/100).format('$0,0.00')}
+        -
+        {moment(createdAt).format('MMMM Do,YYYY')}</p>
         
     </div>
    )
